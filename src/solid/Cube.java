7fcf5c3898acff70feb98b9dcdaa5c87b6e94a6e
mhanus2 <mhmartinhanus@gmail.com>
2024-01-19 23:29:25 +0100

    Added: New points in Cube

--- src/solid/Cube.java
+++ src/solid/Cube.java
@@ -10,13 +10,25 @@ public class Cube extends Solid {
         vb.add(new Point3D(1, 0, 0)); // v1
         vb.add(new Point3D(1, 1, 0)); // v2
         vb.add(new Point3D(0, 1, 0)); // v3
+        vb.add(new Point3D(0, 0, 1)); // v4
+        vb.add(new Point3D(1, 0, 1)); // v5
+        vb.add(new Point3D(1, 1, 1)); // v6
+        vb.add(new Point3D(0, 1, 1)); // v7
 
         // ib
         addIndices(
                 0, 1,
                 1, 2,
                 2, 3,
-                3, 0
+                3, 0,
+                4, 5,
+                5, 6,
+                6, 7,
+                7, 4,
+                0, 4,
+                1, 5,
+                2, 6,
+                3, 7
         );
     }
 }
