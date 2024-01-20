45cd07e4ccc354ecb99e55bad0849d329c4b1025
mhanus2 <mhmartinhanus@gmail.com>
2024-01-20 15:06:46 +0100

    Added: Cutting lines

--- src/renderer/WiredRenderer.java
+++ src/renderer/WiredRenderer.java
@@ -7,7 +7,7 @@ import transforms.Point3D;
 import transforms.Vec3D;
 
 public class WiredRenderer {
-    private LineRasterizer lineRasterizer;
+    private final LineRasterizer lineRasterizer;
     private Mat4 view, proj;
     private final Vec3D windowTransformA = new Vec3D(1, -1, 1);
     private final Vec3D windowTransformB = new Vec3D(1, 1, 0);
@@ -26,44 +26,47 @@ public class WiredRenderer {
             Point3D a = solid.getVb().get(indexA);
             Point3D b = solid.getVb().get(indexB);
 
-            // modelovací tranformace
+            // Modelovací tranformace
             a = a.mul(solid.getModel());
             b = b.mul(solid.getModel());
 
-            // pohledová tranformace
+            // Pohledová tranformace
             a = a.mul(view);
             b = b.mul(view);
 
-            // projekční tranformace
+            // Projekční tranformace
             a = a.mul(proj);
             b = b.mul(proj);
 
-            // TODO: ořezání
+            // Ořezání
+            if (isWithinBounds(a)) {
+                if (isWithinBounds(b)) {
+                    // Dehomogenizace
+                    Vec3D v = new Vec3D();
+                    Vec3D w = new Vec3D();
 
-            // Dehomogenizace
-            Vec3D v = new Vec3D();
-            Vec3D w = new Vec3D();
+                    if (a.dehomog().isPresent())
+                        v = a.dehomog().get();
+                    if (b.dehomog().isPresent())
+                        w = b.dehomog().get();
 
-            if (a.dehomog().isPresent())
-                v = a.dehomog().get();
-            if (b.dehomog().isPresent())
-                w = b.dehomog().get();
+                    // Tranformace do okna obrazovky
+                    Vec3D aTransformed = v.mul(windowTransformA);
+                    aTransformed = aTransformed.add(windowTransformB);
+                    aTransformed = aTransformed.mul(windowTransformC);
 
-            // Tranformace do okna obrazovky
-            Vec3D aTransformed = v.mul(windowTransformA);
-            aTransformed = aTransformed.add(windowTransformB);
-            aTransformed = aTransformed.mul(windowTransformC);
+                    Vec3D bTransformed = w.mul(windowTransformA);
+                    bTransformed = bTransformed.add(windowTransformB);
+                    bTransformed = bTransformed.mul(windowTransformC);
 
-            Vec3D bTransformed = w.mul(windowTransformA);
-            bTransformed = bTransformed.add(windowTransformB);
-            bTransformed = bTransformed.mul(windowTransformC);
-
-            // Rasterizace
-            lineRasterizer.rasterize(
-                    (int)Math.round(aTransformed.getX()), (int)Math.round(aTransformed.getY()),
-                    (int)Math.round(bTransformed.getX()), (int)Math.round(bTransformed.getY()),
-                    solid.getColor()
-            );
+                    // Rasterizace
+                    lineRasterizer.rasterize(
+                            (int) Math.round(aTransformed.getX()), (int) Math.round(aTransformed.getY()),
+                            (int) Math.round(bTransformed.getX()), (int) Math.round(bTransformed.getY()),
+                            solid.getColor()
+                    );
+                }
+            }
         }
     }
 
@@ -73,6 +76,12 @@ public class WiredRenderer {
 
     public void setProj(Mat4 proj) {
         this.proj = proj;
+    }
+
+    private boolean isWithinBounds(Point3D point) {
+        return (-point.getW() <= point.getX() && point.getX() <= point.getW()) &&
+                (-point.getW() <= point.getY() && point.getY() <= point.getW()) &&
+                (0 <= point.getZ() && point.getZ() <= point.getW());
     }
 }
 
